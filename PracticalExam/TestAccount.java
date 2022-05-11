ee87ee1d6004b07e3c788297a53a4c18f72249fb
NguyenDinhThanh29 <103102477+NguyenDinhThanh29@users.noreply.github.com>
2022-05-11 15:57:50 +0700

    Hoàn thành Exam

---
+++ PracticalExam/TestAccount.java
@@ -0,0 +1,8 @@
+public class TestAccount {
+    public static void main(String[] args) {
+        Account m1 = new Account();
+        m1.input();
+        m1.DepositAndWithdraw();
+        System.out.println(m1);
+    }
+}
